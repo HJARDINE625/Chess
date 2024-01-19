bab8740e20eb874d7adbb7520a09f5db3b640022
Hyrum Jardine <hyrum@jardinefamily.local>
2024-01-19 12:24:36 -0800

    Added base classes builds to allow for positions and chessboards Added stuff to chessPeice to test if the peice had moved

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +11,43 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessBoard firstMoveChecker = new ChessBoard();
+
+    public boolean hasThisPieceMoved(ChessPosition currentPosition) {
+        if(hasMoved) {
+            return hasMoved;
+        } else if (this.type!=firstMoveChecker.getPiece(currentPosition).type){
+            hasMoved = true;
+            return hasMoved;
+        } else if (this.pieceColor!=firstMoveChecker.getPiece(currentPosition).pieceColor){
+            hasMoved = true;
+            return hasMoved;
+        } else {
+            return hasMoved;
+        }
+    }
+
+    public void setHasMoved(boolean hasMoved) {
+        this.hasMoved = hasMoved;
+    }
+
+    private boolean hasMoved = false;
+    private ChessGame.TeamColor pieceColor;
+
+    public void setPieceColor(ChessGame.TeamColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public void setType(PieceType type) {
+        this.type = type;
+    }
+
+    private ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.type = type;
+        this.pieceColor = pieceColor;
+        this.firstMoveChecker.resetBoard();
     }
 
     /**
@@ -29,14 +66,16 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -47,6 +86,7 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return new ArrayList<>();
+        //throw new RuntimeException("Not implemented");
     }
 }
