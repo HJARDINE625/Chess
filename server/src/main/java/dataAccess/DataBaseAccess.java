5326063513c5fe21e722c5d4d5c7b5a737e7024e
Hyrum <Hyrum.Jardine@outlook.com>
2024-02-15 00:40:42 -0700

    more updates to base cases

--- server/src/main/java/dataAccess/DataBaseAccess.java
+++ server/src/main/java/dataAccess/DataBaseAccess.java
@@ -3,17 +3,23 @@ package dataAccess;
 import model.Data;
 
 public class DataBaseAccess {
+    //we need to be able to use the database directly and it ought to be the same one each time...
+private DatabaseManager myDataBaseAccess;
 
+//this function simply finds the item from the selected database... this does require a little bit of coupling, because you need to know what kind of database to search for, but it removes some by allowing a generic "data" return type.
 public Data Select(String findMe, String database){
 
     return null;
 }
 
+//find the selected data and then alter it, otherwise passup the not-found data error on the chain.
 public Data Alter(String findMe, String database, String becomeMe){
     Select(findMe, database);
 
     return null;
 }
+
+//find and destory the selected data.
 
 public Data Delete(String findMe, String database) {
     Alter(findMe, database, null);
@@ -43,9 +49,12 @@ public Data DeleteALL(String database){
     return null;
 }
 
+//this destores everything in the selected database, call it a few times to destory all three databases.
 public Data[] SelectALL(String database){
     return null;
 }
+
+//find out what is in the DATA that is passed around.
 
 private String unpack() {
     return null;
--- server/src/main/java/service/RegistrationService.java
+++ server/src/main/java/service/RegistrationService.java
@@ -1,11 +1,20 @@
 package service;
 
+import dataAccess.DataBaseAccess;
 import model.AuthData;
+import model.Data;
 import model.UserData;
 
 public class RegistrationService {
 
+        public DataBaseAccess myDataBaseAcess;
     public AuthData register(UserData user){
+        //somehow we need to access the same database every time we add a user.
+            Data userInfo = myDataBaseAcess.Add(user.username(), "UserName", true);
+            if(user.getMyUserData() == null){
+                Throw Error;
+            }
+
 
     }
 
