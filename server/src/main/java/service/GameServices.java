9c1c81147dd61087f8b12f663da1f6ed77627068
Hyrum <Hyrum.Jardine@outlook.com>
2024-03-04 16:42:07 -0700

    All the logic should now check out (although it is possible the test cases have symbols that will not be allowed...)

--- server/src/main/java/service/GameServices.java
+++ server/src/main/java/service/GameServices.java
@@ -7,37 +7,51 @@ import model.*;
 
 public class GameServices {
     //return all games that are currently in the database.
-    public Responses listGames(String authToken, DataAccesser myDatabase){
+    public Responses listGames(String authToken, DataAccesser myDatabase) {
         //This line, BY ITSELF, should be able to verify if I can look at this data
-        if(!myDatabase.checkAuthorization(authToken)){
-            Responses faliureResponse = new Responses(401);
-            faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+        try {
+            if (!myDatabase.checkAuthorization(authToken)) {
+                Responses faliureResponse = new Responses(401);
+                faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+                return faliureResponse;
+            }
+            //If we are still here then we are authorized...
+            //now we need to get all games returned
+            GameData[] allGames = myDatabase.listGames();
+            //If there are no games, send back what was asked for... nothing...
+            if (allGames == null) {
+                Responses emptyResponse = new Responses(200);
+                return emptyResponse;
+            }
+            //otherwise transfer the games...
+            Responses sucessResponse = new Responses(200);
+            sucessResponse.setAllGames(allGames);
+            return sucessResponse;
+        } catch (DataAccessException e){
+            Responses faliureResponse = new Responses(500);
+            faliureResponse.setMyException(e);
             return faliureResponse;
         }
-        //If we are still here then we are authorized...
-        //now we need to get all games returned
-        GameData[] allGames = myDatabase.listGames();
-        //If there are no games, send back what was asked for... nothing...
-        if(allGames == null){
-            Responses emptyResponse = new Responses(200);
-            return emptyResponse;
-        }
-        //otherwise transfer the games...
-        Responses sucessResponse = new Responses(200);
-        sucessResponse.setAllGames(allGames);
-        return sucessResponse;
     }
 
     //create a new game...
     public Responses createGame(String authToken, String name, DataAccesser myDatabase){
-        //This line, BY ITSELF, should be able to verify if I can look at this data
-        if(!myDatabase.checkAuthorization(authToken)){
-            Responses faliureResponse = new Responses(401);
-            faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+        //declare here for things to work...
+        GameData game;
+        try {
+            //This line, BY ITSELF, should be able to verify if I can look at this data
+            if (!myDatabase.checkAuthorization(authToken)) {
+                Responses faliureResponse = new Responses(401);
+                faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+                return faliureResponse;
+            }
+            //Now I need to make the new game
+            game = myDatabase.createGame(name);
+        } catch (DataAccessException e){
+            Responses faliureResponse = new Responses(500);
+            faliureResponse.setMyException(e);
             return faliureResponse;
         }
-        //Now I need to make the new game
-        GameData game = myDatabase.createGame(name);
         //and now I need to make the response method...
         Responses sucessResponse = new Responses(200);
         sucessResponse.setMyGameData(game);
@@ -46,37 +60,45 @@ public class GameServices {
 
     //Finally join a game!
     public Responses joinGame(String authToken, int gameID, String joinAsColor, DataAccesser myDatabase){
-        //This line, BY ITSELF, should be able to verify if I can look at this data
-        if(!myDatabase.checkAuthorization(authToken)){
-            Responses faliureResponse = new Responses(401);
-            faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
-            return faliureResponse;
-        }
-        //now I need to make sure that the game asked for exists.
-        if(!myDatabase.locateGameID(gameID)){
-            Responses faliureResponse = new Responses(400);
-            //I think this is the kind of error they want, they may want 500 or something...
-            faliureResponse.setMyException(new DataAccessException("Error: bad request"));
-            return faliureResponse;
-        }
-        //Looks like both the game and the AuthToken check out...
+        //need to declare this for things to work...
+        GameData updatedGame;
+        try {
+            //This line, BY ITSELF, should be able to verify if I can look at this data
+            if (!myDatabase.checkAuthorization(authToken)) {
+                Responses faliureResponse = new Responses(401);
+                faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+                return faliureResponse;
+            }
+            //now I need to make sure that the game asked for exists.
+            if (!myDatabase.locateGameID(gameID)) {
+                Responses faliureResponse = new Responses(400);
+                //I think this is the kind of error they want, they may want 500 or something...
+                faliureResponse.setMyException(new DataAccessException("Error: bad request"));
+                return faliureResponse;
+            }
+            //Looks like both the game and the AuthToken check out...
 
-        //Now we need to see if the game can be joined as asked...
-        if(!myDatabase.colorExists(joinAsColor, gameID)){
+            //Now we need to see if the game can be joined as asked...
+            if (!myDatabase.colorExists(joinAsColor, gameID)) {
+                Responses faliureResponse = new Responses(500);
+                //I think this is the kind of error they want, they may want 400 or something...
+                faliureResponse.setMyException(new DataAccessException("Error: no such team color!"));
+                return faliureResponse;
+            }
+            //The color exists... is it available?
+            if (!myDatabase.colorNotTaken(joinAsColor, gameID)) {
+                Responses faliureResponse = new Responses(403);
+                //I think this is the kind of error they want, they may want 500 or something...
+                faliureResponse.setMyException(new DataAccessException("Error: already taken"));
+                return faliureResponse;
+            }
+            //Apparently it is available... now I should return something...
+            updatedGame = myDatabase.updateGame(gameID, joinAsColor, authToken);
+        } catch (DataAccessException e){
             Responses faliureResponse = new Responses(500);
-            //I think this is the kind of error they want, they may want 400 or something...
-            faliureResponse.setMyException(new DataAccessException("Error: no such team color!"));
+            faliureResponse.setMyException(e);
             return faliureResponse;
         }
-        //The color exists... is it available?
-        if(!myDatabase.colorNotTaken(joinAsColor, gameID)){
-            Responses faliureResponse = new Responses(403);
-            //I think this is the kind of error they want, they may want 500 or something...
-            faliureResponse.setMyException(new DataAccessException("Error: already taken"));
-            return faliureResponse;
-        }
-        //Apparently it is available... now I should return something...
-        GameData updatedGame = myDatabase.updateGame(gameID, joinAsColor, authToken);
         Responses successResponse = new Responses(200);
         successResponse.setMyGameData(updatedGame);
         return successResponse;
--- server/src/main/java/service/RegistrationServices.java
+++ server/src/main/java/service/RegistrationServices.java
@@ -13,17 +13,24 @@ public class RegistrationServices {
         //check that we have everything a user should have
 
         //now check that the data works
-        if(!myDatabase.locateUsername(user.username())){
-            UserData newUser = myDatabase.createUser(user.username(), user.password(), user.email());
-            AuthData loginAuthentication = myDatabase.createAuth(newUser.username());
-            //Now we have the user input to the database and a new login token for them, so we should return it in a response...
-            Responses successResponse = new Responses(200);
-            successResponse.setMyAuthData(loginAuthentication);
-            return successResponse;
-        } else {
-            //If we got here we already have that user
-            Responses faliureResponse = new Responses(403);
-            faliureResponse.setMyException(new DataAccessException("Error: already taken"));
+        try {
+            if (!myDatabase.locateUsername(user.username())) {
+                UserData newUser = myDatabase.createUser(user.username(), user.password(), user.email());
+                AuthData loginAuthentication = myDatabase.createAuth(newUser.username());
+                //Now we have the user input to the database and a new login token for them, so we should return it in a response...
+                Responses successResponse = new Responses(200);
+                successResponse.setMyAuthData(loginAuthentication);
+                return successResponse;
+            } else {
+                //If we got here we already have that user
+                Responses faliureResponse = new Responses(403);
+                faliureResponse.setMyException(new DataAccessException("Error: already taken"));
+                return faliureResponse;
+            }
+            //we add the below in order to make sure that we can catch database errors...
+        } catch (DataAccessException e){
+            Responses faliureResponse = new Responses(500);
+            faliureResponse.setMyException(e);
             return faliureResponse;
         }
 
@@ -34,20 +41,26 @@ public class RegistrationServices {
     public Responses login(String username, String password, DataAccesser myDatabase){
         //I think there will only be two elements used from this database (having not yet made it)
         //Then it will add an AuthToken to a new database based off of it, but only if the username and password match...
-        UserData thisUser = myDatabase.getUser(username, password);
-        //check if we got a null user, if so this actually just threw an error...
-        if(thisUser == null){
-            Responses faliureResponse = new Responses(401);
-            faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+        try {
+            UserData thisUser = myDatabase.getUser(username, password);
+            //check if we got a null user, if so this actually just threw an error...
+            if (thisUser == null) {
+                Responses faliureResponse = new Responses(401);
+                faliureResponse.setMyException(new DataAccessException("Error: unauthorized"));
+                return faliureResponse;
+            } else {
+                //We got something back, which means the person was found...
+                AuthData authenticationToken = myDatabase.createAuth(username);
+                Responses successResponse = new Responses(200);
+                successResponse.setMyAuthData(authenticationToken);
+                return successResponse;
+            }
+            //we could not access the database at some point
+        } catch (DataAccessException e){
+            Responses faliureResponse = new Responses(500);
+            faliureResponse.setMyException(e);
             return faliureResponse;
-        } else{
-            //We got something back, which means the person was found...
-            AuthData authenticationToken = myDatabase.createAuth(username);
-            Responses successResponse = new Responses(200);
-            successResponse.setMyAuthData(authenticationToken);
-            return successResponse;
         }
-
     }
 
     //this destroys an authtoken...
@@ -55,14 +68,21 @@ public class RegistrationServices {
         //We need to make sure that the authToken is real and really applies to the person... cancel the second part...
         //they do not seem to care about mentioning the person beyond authentication...
         //We can do this at the same time as removing that authorization by using a special DataAccessor function
-        if(myDatabase.deleteAuth(authentication)){
-            return new Responses(200);
-        } else {
-            //clearly we could not find the thing to delete
-            //Strictly speaking, I think this is the wrong error, but it is the only named kind they wanted us to return...
-            Responses mistake = new Responses(401);
-            mistake.setMyException(new DataAccessException("Error: unauthorized"));
-            return mistake;
+        try {
+            if (myDatabase.deleteAuth(authentication)) {
+                return new Responses(200);
+            } else {
+                //clearly we could not find the thing to delete
+                //Strictly speaking, I think this is the wrong error, but it is the only named kind they wanted us to return...
+                Responses mistake = new Responses(401);
+                mistake.setMyException(new DataAccessException("Error: unauthorized"));
+                return mistake;
+            }
+            //this should make sure that if the database threw an error it still does not matter to the logic...
+        } catch (DataAccessException e){
+            Responses faliureResponse = new Responses(500);
+            faliureResponse.setMyException(e);
+            return faliureResponse;
         }
     }
 
