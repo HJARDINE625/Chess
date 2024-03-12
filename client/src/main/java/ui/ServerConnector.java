c2a4c4b65140ab9f527ee45bceb95c2c42b30991
Hyrum <Hyrum.Jardine@outlook.com>
2024-03-11 22:15:32 -0600

    Added some code to write out messages and draw boards, not sure if it is up to code...

--- client/src/main/java/ui/ServerConnector.java
+++ client/src/main/java/ui/ServerConnector.java
@@ -1,14 +1,25 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import static ui.EscapeSequences.*;
+
 public class ServerConnector {
+
+    private PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
 
     //need some kind of init and stop functions
 
@@ -88,19 +99,19 @@ public class ServerConnector {
                         for (GameData g: games) {
                             message = message + "[" + gameNumber + "] : " + "(name) : ";
                             if(g.gameName() != null) {
-                                message = message + g.gameName() + " ";
+                                message = message + g.gameName() + " (White Username) : ";
                             }  else {
-                                message = message + "NONE! ";
+                                message = message + "NONE!  (White Username) : ";
                             }
                             if(g.whiteUsername() != null) {
-                                message = message + g.whiteUsername() + " ";
+                                message = message + g.whiteUsername() + " (Black Username) : ";
                             }  else {
-                                message = message + "NONE! ";
+                                message = message + "NONE! (Black Username) : ";
                             }
                             if(g.blackUsername() != null) {
                                 message = message + g.blackUsername() + " ";
                             }  else {
-                                message = message + "NONE! ";
+                                message = message + "NONE! \n";
                             }
                         }
                         message = message + json;
@@ -131,7 +142,103 @@ public class ServerConnector {
             completeAction(0);
         }
         //Now print the message to terminal...
+        out.print(SET_TEXT_COLOR_MAGENTA);
+        out.print(SET_TEXT_BOLD);
+        out.print(SET_BG_COLOR_DARK_GREEN);
+        out.print(message);
     }
+
+    //here I pass in a game specifically so I can do something more complicated if I choose to...
+    private void draw(ChessGame game) {
+        ChessBoard board = game.getBoard();
+        //give a line or so
+        out.print(SET_BG_COLOR_BLUE);
+        for(int size = 0; size < board.getChessBoardSize(); ++size) {
+            out.print(EMPTY);
+        }
+        boolean blackSpace = false;
+        //print right side up board
+        for(int space = 0; space < board.getChessBoardSize(); ++space) {
+            for(int width = 0; width < board.getChessBoardSize(); ++width) {
+                if(blackSpace){
+                    blackSpace = false;
+                    out.print(SET_BG_COLOR_WHITE);
+                } else {
+                    blackSpace = true;
+                    out.print(SET_BG_COLOR_BLACK);
+                }
+                if (board.getPiece(new ChessPosition(space, width)) != null) {
+                    ChessPiece myPiece = board.getPiece(new ChessPosition(space, width));
+                    ChessGame.TeamColor color = myPiece.getTeamColor();
+                    ChessPiece.PieceType piece = myPiece.getPieceType();
+                    replace(color.name(), piece.name());
+                } else {
+                    out.print(EMPTY);
+                }
+            }
+        }
+        //give a line or so
+        out.print(SET_BG_COLOR_BLUE);
+        for(int size = 0; size < board.getChessBoardSize(); ++size) {
+            out.print(EMPTY);
+        }
+        blackSpace = false;
+        //print upside down board
+        for(int space = board.getChessBoardSize(); space > 0; --space) {
+            for(int width = board.getChessBoardSize(); width > 0; --width) {
+                if(blackSpace){
+                    blackSpace = false;
+                    out.print(SET_BG_COLOR_WHITE);
+                } else {
+                    blackSpace = true;
+                    out.print(SET_BG_COLOR_BLACK);
+                }
+                if (board.getPiece(new ChessPosition(space, width)) != null) {
+                    ChessPiece myPiece = board.getPiece(new ChessPosition(space, width));
+                    ChessGame.TeamColor color = myPiece.getTeamColor();
+                    ChessPiece.PieceType piece = myPiece.getPieceType();
+                    replace(color.name(), piece.name());
+                } else {
+                    out.print(EMPTY);
+                }
+            }
+        }
+    }
+
+    private void replace(String color, String piece){
+        //right now we only have escape sequences for black and white...
+        if(color.equals("BLACK")) {
+        if(piece.equals("PAWN")){
+            out.print(BLACK_PAWN);
+        } else if(piece.equals("KNIGHT")){
+            out.print(BLACK_KNIGHT);
+        } else if(piece.equals("BISHOP")){
+            out.print(BLACK_BISHOP);
+        } else if(piece.equals("ROOK")){
+            out.print(BLACK_ROOK);
+        } else if(piece.equals("QUEEN")){
+            out.print(BLACK_QUEEN);
+        } else if(piece.equals("KING")){
+            out.print(BLACK_KING);
+        }
+        } else if(color.equals("WHITE")){
+            if(piece.equals("PAWN")){
+                out.print(WHITE_PAWN);
+            } else if(piece.equals("KNIGHT")){
+                out.print(WHITE_KNIGHT);
+            } else if(piece.equals("BISHOP")){
+                out.print(WHITE_BISHOP);
+            } else if(piece.equals("ROOK")){
+                out.print(WHITE_ROOK);
+            } else if(piece.equals("QUEEN")){
+                out.print(WHITE_QUEEN);
+            } else if(piece.equals("KING")){
+                out.print(WHITE_KING);
+            }
+        }
+    }
+
+
 
     private String help(){
         String returnSupport = new String();
